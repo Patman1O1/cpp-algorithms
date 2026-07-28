57c6be71bc26f15bff048af8c544f538184b786c
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-28 12:10:33 -0500

    Include headers and implement namespace

---
+++ benchmarks/cpp_algorithms_benchmark.cpp
@@ -0,0 +1,13 @@
+// ISO C Includes
+
+// ISO C++ Includes
+
+// Google Benchmark Includes
+#include <benchmark/benchmark.h>
+
+// Local Includes
+#include <cpp_algorithms/cpp_algorithms.hpp>
+
+namespace cpp_algorithms::benchmarks {
+
+} // namespace cpp_algorithms::benchmarks
